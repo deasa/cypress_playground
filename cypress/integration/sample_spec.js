dddd1817af8db7bf033e3fdc3bf2be56cd126053
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-03-26 23:05:18 -0600

    removed sample tests renamed existing tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,26 +1,16 @@
-describe('My first sample test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(true)
-    })
-} )
-
-describe('My first failing sample test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(false)
-    })
-})
-
-describe('My first EMBR test', () => {
+describe('EMBR Smoke Test', () => {
     it('Visits the embr app', () => {
         cy.visit('http://test.embr.biofiredx.net');
     });
 });
 
-describe('Login to application', () => {
+describe('Login Page Test', () => {
     it('Logs in to EMBR', () => {
         cy.visit('http://test.embr.biofiredx.net');
 
         cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]`).type('ldaptest');
         cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input`).type('drowssap');
+        cy.xpath(`//button[@label = 'Log In']`).click();
+        cy.url().should('include', 'home')
     });
 });

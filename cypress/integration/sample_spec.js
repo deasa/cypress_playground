602149046b6d75fc7b0873ff2fca00b875cfacd6
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-03-26 22:41:34 -0600

    adding cypress-xpath and writing first typing test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -9,3 +9,18 @@ describe('My first failing sample test', function() {
         expect(true).to.equal(false)
     })
 })
+
+describe('My first EMBR test', () => {
+    it('Visits the embr app', () => {
+        cy.visit('http://test.embr.biofiredx.net');
+    });
+});
+
+describe('Login to application', () => {
+    it('Logs in to EMBR', () => {
+        cy.visit('http://test.embr.biofiredx.net');
+
+        cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]`).type('ldaptest');
+        cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input`).type('drowssap');
+    });
+});

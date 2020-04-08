0e0606df06d43f8d8120cb82fcab340014180c7e
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-08 00:12:19 -0600

    increased robustness of the can log in to application method

--- cypress/integration/loginPage_spec.js
+++ cypress/integration/loginPage_spec.js
@@ -1,4 +1,8 @@
+'use strict';
 /// <reference types="Cypress" />
+const xUsernameInput = "//input[preceding-sibling::label[text() = 'Username']][1]";
+const xPasswordInput = "//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input";
+const xLoginButton = "//button[@label = 'Log In']";
 
 describe('The EMBR application', () => {
     it('successfully loads', () => {
@@ -6,19 +10,21 @@ describe('The EMBR application', () => {
     });
 });
 
-describe('Items On Login Page', () => {
-    it('All three login items are visible on the page', () => {
+describe('Login Page', () => {
+    it('has all three login items visible on the page', () => {
 
     })
 })
 
-describe('Login Page Test', () => {
-    it('Logs in to EMBR', () => {
+describe('Login Page', () => {
+    it('can log in to application', () => {
         cy.visit('/');
 
-        cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]`).type('ldaptest');
-        cy.xpath(`//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input`).type('drowssap');
-        cy.xpath(`//button[@label = 'Log In']`).click();
+        cy.xpath(`${xUsernameInput}`).type('ldaptest');
+        cy.xpath(`${xPasswordInput}`).type('drowssap');
+        cy.xpath(`${xLoginButton}`).click();
         cy.url().should('include', 'home')
+        cy.contains('Welcome').should('contain.text', 'LDAP');
+        //expect(window.localStorage.getItem('currentUser')).to.have.property("AccountName", "ldaptest");
     });
 });

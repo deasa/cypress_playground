8d1b28f817540b40be6b0289e55fca17404ee70e
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-08 11:15:35 -0600

    refactored to put under one parent describe added invalid test

--- cypress/integration/loginPage_spec.js
+++ cypress/integration/loginPage_spec.js
@@ -1,32 +1,41 @@
 'use strict';
-/// <reference types="Cypress" />
 const xUsernameInput = "//input[preceding-sibling::label[text() = 'Username']][1]";
 const xPasswordInput = "//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input";
 const xLoginButton = "//button[@label = 'Log In']";
 
-describe('The EMBR application', () => {
+describe('Login Page', () => {
+    
     it('successfully loads', () => {
         cy.visit('/');
     });
-});
 
-describe('Login Page', () => {
     it('has all three login items visible on the page', () => {
         cy.xpath(`${xUsernameInput}`).should('be.visible')
         cy.xpath(`${xPasswordInput}`).should('be.visible');
         cy.xpath(`${xLoginButton}`).should('be.visible');
-    })
-})
+    });
 
-describe('Login Page', () => {
-    it('can log in to application', () => {
-        cy.visit('/');
+    describe('Login Page _ login', () => {
+        const validUserName = "ldaptest";
+        const validPassword = "drowssap";
 
-        cy.xpath(`${xUsernameInput}`).type('ldaptest');
-        cy.xpath(`${xPasswordInput}`).type('drowssap');
-        cy.xpath(`${xLoginButton}`).click();
-        cy.url().should('include', 'home')
-        cy.contains('Welcome').should('contain.text', 'LDAP');
-        //expect(window.localStorage.getItem('currentUser')).to.have.property("AccountName", "ldaptest");
+        beforeEach(() => {
+            cy.reload(true);
+        })
+
+        it('can log in to application _ ldaptest', () => {
+            cy.xpath(`${xUsernameInput}`).type(validUserName);
+            cy.xpath(`${xPasswordInput}`).type(validPassword);
+            cy.xpath(`${xLoginButton}`).click();
+            cy.url().should('include', 'home');
+            cy.contains('Welcome').should('contain.text', 'LDAP');
+        });
+        it('rejects invalid username', () => {
+            cy.xpath(`${xUsernameInput}`).type('invalidUsername');
+            cy.xpath(`${xPasswordInput}`).type(validPassword);
+            cy.xpath(`${xLoginButton}`).click();
+            //expect(cy.get('.errorMessage').to.have.text('Confirm credentials are correct');
+            cy.get('.errorMessage').should('contain.text', 'Confirm credentials are correct');
+        });
     });
 });

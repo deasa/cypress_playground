f968c7c57aba3f5c614c142016f029350ba991d3
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-08 23:21:44 -0600

    finished writing login page tests. 25 seconds.

--- cypress/integration/loginPage_spec.js
+++ cypress/integration/loginPage_spec.js
@@ -1,41 +1,57 @@
 'use strict';
-const xUsernameInput = "//input[preceding-sibling::label[text() = 'Username']][1]";
-const xPasswordInput = "//input[preceding-sibling::label[text() = 'Username']][1]/following-sibling::input";
-const xLoginButton = "//button[@label = 'Log In']";
 
 describe('Login Page', () => {
-    
-    it('successfully loads', () => {
+    beforeEach(function() {
         cy.visit('/');
+        cy.get('input[pinputtext]').as('usernameInput');
+        cy.get('input[ppassword]').as('passwordInput');
+        cy.get("button[label='Log In']").as('loginButton');
+    });
+
+    it('successfully loads', () => {
+        cy.get('@usernameInput').should('be.visible');
     });
 
     it('has all three login items visible on the page', () => {
-        cy.xpath(`${xUsernameInput}`).should('be.visible')
-        cy.xpath(`${xPasswordInput}`).should('be.visible');
-        cy.xpath(`${xLoginButton}`).should('be.visible');
+        cy.get('@usernameInput').should('be.visible')
+        cy.get('@passwordInput').should('be.visible');
+        cy.get('@loginButton').should('be.visible');
     });
 
     describe('Login Page _ login', () => {
         const validUserName = "ldaptest";
         const validPassword = "drowssap";
 
-        beforeEach(() => {
-            cy.reload(true);
-        })
-
         it('can log in to application _ ldaptest', () => {
-            cy.xpath(`${xUsernameInput}`).type(validUserName);
-            cy.xpath(`${xPasswordInput}`).type(validPassword);
-            cy.xpath(`${xLoginButton}`).click();
+            cy.get('@usernameInput').type(validUserName);
+            cy.get('@passwordInput').type(validPassword);
+            cy.get('@loginButton').click();
             cy.url().should('include', 'home');
             cy.contains('Welcome').should('contain.text', 'LDAP');
         });
+
         it('rejects invalid username', () => {
-            cy.xpath(`${xUsernameInput}`).type('invalidUsername');
-            cy.xpath(`${xPasswordInput}`).type(validPassword);
-            cy.xpath(`${xLoginButton}`).click();
-            //expect(cy.get('.errorMessage').to.have.text('Confirm credentials are correct');
+            cy.get('@usernameInput').type('badUsername');
+            cy.get('@passwordInput').type(validPassword);
+            cy.get('@loginButton').click();
             cy.get('.errorMessage').should('contain.text', 'Confirm credentials are correct');
+        });
+
+        it('rejects invalid password', () => {
+            cy.get('@usernameInput').type(validUserName);
+            cy.get('@passwordInput').type('badPassword');
+            cy.get('@loginButton').click();
+            cy.get('.errorMessage').should('contain.text', 'Confirm credentials are correct');
+        });
+
+        it('log in button not enabled until after username and password are entered', function() {
+            cy.get('@loginButton').should('be.disabled');
+            cy.get('@usernameInput').type(validUserName);
+            cy.get('@loginButton').should('be.disabled');
+            cy.get('@passwordInput').type(validPassword);
+            cy.get('@loginButton').should('not.be.disabled');
+            cy.get('@usernameInput').clear();
+            cy.get('@loginButton').should('be.disabled');
         });
     });
 });

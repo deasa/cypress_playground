c69f2fb07ccb8e7ee0a3cbe55acf74b0fb2f7836
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-10 23:20:09 -0600

    stubbed getAllMasterBatchRecordsNoModules and cleaned up loginPage_spec Should be a finalized version of the loginPage_spec Could think about not reloading before each test.

--- cypress/integration/loginPage_spec.js
+++ cypress/integration/loginPage_spec.js
@@ -2,14 +2,9 @@
 
 describe('Login Page', () => {
     beforeEach(function() {
-        console.log('in the before each!');
         cy.server();
-        cy.fixture('ldapuser.json').as('ldapUser');
-        cy.route('POST', '/ESig/SecureVerifyWithGroups', 'fixture:ldapuser.json');
-        cy.route(/getAllMasterBatch/).as('getAllMbrs');
-        cy.route(/GetPwUrl/, "\"http://blah.pw.biofiredx.net/services/\"").as('getPWUrl');
+        cy.route(/getAllMasterBatch/, 'fixture:allMBRsNoModules.json').as('getAllMbrs');
         cy.visit('/');
-        // cy.wait('@getPWUrl');
         cy.get('input[pinputtext]').as('usernameInput');
         cy.get('input[ppassword]').as('passwordInput');
         cy.get("button[label='Log In']").as('loginButton');
@@ -18,10 +13,6 @@ describe('Login Page', () => {
     it('successfully loads', () => {
         cy.get('@usernameInput').should('be.visible');
     });
-
-    it('Can hit a custom command', function() {
-        cy.printToConsole();
-    })
 
     it('has all three login items visible on the page', () => {
         cy.get('@usernameInput').should('be.visible')
@@ -33,11 +24,11 @@ describe('Login Page', () => {
         const validUserName = "ldaptest";
         const validPassword = "drowssap";
 
-        it.only('can log in to application _ ldaptest', () => {
+        it('can log in to application _ ldaptest', () => {
             cy.get('@usernameInput').type(validUserName);
             cy.get('@passwordInput').type(validPassword);
             cy.get('@loginButton').click();
-            cy.wait('@getAllMbrs', {timeout: 60000});
+            cy.wait('@getAllMbrs');
             cy.url().should('include', 'home');
             cy.contains('Welcome').should('contain.text', 'LDAP');
         });

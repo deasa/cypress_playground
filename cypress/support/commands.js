066a602471f160f26ca823c31be77015478a29ec
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-14 23:21:08 -0600

    added more loginAs commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,31 @@
 Cypress.Commands.add('loginAsAdmin', function() {
-    cy.clearLocalStorage();
-    let d = new Date;
-    let nowDate = d.toISOString();
-    let data = cy.fixture('ldapuser').then((user) => {
-        user._dateGenerated = nowDate;
-        user._accessLevel = "Admin"
-    });
-    console.log(data);
-    window.localStorage.setItem('currentUser', data);
+    loginAs("Admin");
 });
 
+Cypress.Commands.add('loginAsSupervisor', function() {
+    loginAs("Supervisor");
+})
+
+Cypress.Commands.add('loginAsQa', function() {
+    loginAs("DocumentControl");
+})
+
+Cypress.Commands.add('loginAsGeneric', function() {
+    loginAs("");
+})
+
+function loginAs(userType){
+    console.log('logging in as ' + userType)
+    let d = new Date;
+    let nowDate = d.toISOString();
+    cy.readFile('cypress/fixtures/ldapuser.json').then((str) => {
+        str.Item["_dateGenerated"] = nowDate;
+        str.Item["_accessLevel"] = userType;
+        window.localStorage.setItem('currentUser', JSON.stringify(str.Item));
+    });
+}
+
 Cypress.Commands.add('printToConsole', function() {
-    console.log('YOU HIT A CUSTOM COMMAND!!!');
+    // console.log('YOU HIT A CUSTOM COMMAND!!!');
+    console.log(window.localStorage.getItem('currentUser'));
 });

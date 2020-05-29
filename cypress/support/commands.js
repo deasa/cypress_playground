302f260efb40259ced35554b148bb19bfe785abe
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-05-29 01:20:54 -0600

    updated login as functions to work with the new JWT login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,30 @@
 Cypress.Commands.add('loginAsAdmin', function() {
-    loginAs("Admin", "Admin");
+    loginAs('ldaptest', 'drowssap')
 });
 
 Cypress.Commands.add('loginAsSupervisor', function() {
-    loginAs("Supervisor", "Supervisor");
+    loginAs("ldaptest2", "drowssap");
 })
 
 Cypress.Commands.add('loginAsQa', function() {
-    loginAs("DocumentControl", "QA");
+    loginAs("ldaptest3", "drowssap");
 })
 
 Cypress.Commands.add('loginAsGeneric', function() {
-    loginAs("", "Generic");
+    loginAs("ldapCyber", "drowssap");
 })
 
-function loginAs(userType, userName){
-    console.log('logging in as ' + userType)
-    let d = new Date;
-    let nowDate = d.toISOString();
-    cy.readFile('cypress/fixtures/ldapuser.json').then(function(str) {
-        str.Item["_dateGenerated"] = nowDate;
-        str.Item["_accessLevel"] = userType;
-        str.Item.UserName = userName;
-        window.localStorage.setItem('currentUser', JSON.stringify(str.Item));
-    });
-}
+Cypress.Commands.add('getJwtToken', function(uname, pword) {
+    console.log('getting jwt token for ' + uname + ' : ' + pword)
+    cy.request('POST', 'http://test.manufacturingservice.biofiredx.net//EmbrEsig/VerifyAndGenerateJwt', {Username: uname, Password: pword}).then(function(response) {
+        console.log('got token: ' + response.body.Item)
+        return response.body.Item
+    })
+})
 
-Cypress.Commands.add('printToConsole', function() {
-    // console.log('YOU HIT A CUSTOM COMMAND!!!');
-    console.log(window.localStorage.getItem('currentUser'));
-});
+function loginAs(username, password){
+    console.log('logging in as ' + username + ' : ' + password)
+    cy.getJwtToken(username, password).then(function(token) {
+        window.localStorage.setItem('currentUser', JSON.stringify(token))
+    })
+}

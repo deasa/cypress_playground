2d69011b51d2cdf255ae5ce53b81999840a14a8d
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-06-17 16:07:25 -0600

    prettier extension formatting changes added a comment to loginPage_spec

--- cypress/integration/homePage/table/homePage_table_data_spec.js
+++ cypress/integration/homePage/table/homePage_table_data_spec.js
@@ -1,28 +1,34 @@
 import { selectors } from "../homePageCommon.js";
 
-describe('Home Page Table Data', function() {
-    before(function() {
-        cy.loginAsAdmin();
-        cy.server();
-        cy.fixture('allMBRsNoModules_placeholderMBR.json').as('placeholderMBR').then(function(json) {
-            cy.route(/getAllMasterBatch/, json).as('getAllMbrs');
-        })
-        cy
-            .visit('/')
-            .then(function() {
-                cy.get("#EMBR_HomeTable tbody").as('homeTable');
-            });
+describe("Home Page Table Data", function () {
+  before(function () {
+    cy.loginAsAdmin();
+    cy.server();
+    cy.fixture("allMBRsNoModules_placeholderMBR.json")
+      .as("placeholderMBR")
+      .then(function (json) {
+        cy.route(/getAllMasterBatch/, json).as("getAllMbrs");
+      });
+    cy.visit("/").then(function () {
+      cy.get("#EMBR_HomeTable tbody").as("homeTable");
     });
+  });
 
-    //TC-EMBRUI0610-01
-    it('displays data in the correct columns', function() {
-        const mbr = this.placeholderMBR[0];
-        cy.get(selectors.partNumberCell).should('have.text', mbr.PartNumber);
-        cy.get(selectors.partDescriptionCell).should('have.text', mbr.PartDescription);
-        cy.get(selectors.productionVersionCell).should('have.text', mbr.ProductionVersion);
-        cy.get(selectors.revisionNumberCell).should('have.text', mbr.Revision);
-        cy.get(selectors.revisionNameCell).should('have.text', mbr.RevisionName);
-        cy.get(selectors.documentNameCell).should('have.text', mbr.DocumentName);
-        cy.get(selectors.statusCell).should('have.text', mbr.Status);
-    });
+  //TC-EMBRUI0610-01
+  it("displays data in the correct columns", function () {
+    const mbr = this.placeholderMBR[0];
+    cy.get(selectors.partNumberCell).should("have.text", mbr.PartNumber);
+    cy.get(selectors.partDescriptionCell).should(
+      "have.text",
+      mbr.PartDescription
+    );
+    cy.get(selectors.productionVersionCell).should(
+      "have.text",
+      mbr.ProductionVersion
+    );
+    cy.get(selectors.revisionNumberCell).should("have.text", mbr.Revision);
+    cy.get(selectors.revisionNameCell).should("have.text", mbr.RevisionName);
+    cy.get(selectors.documentNameCell).should("have.text", mbr.DocumentName);
+    cy.get(selectors.statusCell).should("have.text", mbr.Status);
+  });
 });
--- cypress/integration/loginPage_spec.js
+++ cypress/integration/loginPage_spec.js
@@ -1,68 +1,78 @@
-'use strict';
+"use strict";
 
 const selectors = {
-    usernameInput: 'input[pinputtext]',
-    passwordInput: 'input[ppassword]',
-    loginButton: "button[label='Log In']",
-    errorMessage: "label[class='errorMessage']"
-}
+  usernameInput: "input[pinputtext]",
+  passwordInput: "input[ppassword]",
+  loginButton: "button[label='Log In']",
+  errorMessage: "label[class='errorMessage']",
+};
 
-describe('Login Page', function() {
-    beforeEach(function() {
-        cy.server();
-        cy.route(/getAllMasterBatch/, 'fixture:allMBRsNoModules.json').as('getAllMbrs');
-        cy.route(/VerifyAndGenerateJwt/).as('generateJwt');
-        cy.visit('/');
-        cy.get(selectors.usernameInput).as('usernameInput');
-        cy.get(selectors.passwordInput).as('passwordInput');
-        cy.get(selectors.loginButton).as('loginButton');
+describe("Login Page", function () {
+  beforeEach(function () {
+    cy.server();
+    cy.route(/getAllMasterBatch/, "fixture:allMBRsNoModules.json").as(
+      "getAllMbrs"
+    );
+    cy.route(/VerifyAndGenerateJwt/).as("generateJwt");
+    cy.visit("/");
+    cy.get(selectors.usernameInput).as("usernameInput");
+    cy.get(selectors.passwordInput).as("passwordInput");
+    cy.get(selectors.loginButton).as("loginButton");
+  });
+
+  it("successfully loads", function () {
+    cy.get("@usernameInput").should("be.visible");
+  });
+
+  it("has all three login items visible on the page", function () {
+    cy.get("@usernameInput").should("be.visible");
+    cy.get("@passwordInput").should("be.visible");
+    cy.get("@loginButton").should("be.visible");
+  });
+
+  describe("Login Page _ login", function () {
+    const validUserName = "ldaptest";
+    const validPassword = "drowssap";
+
+    it("can log in to application _ ldaptest", function () {
+      cy.get("@usernameInput").type(validUserName);
+      cy.get("@passwordInput").type(validPassword);
+      cy.get("@loginButton").click();
+      cy.wait("@getAllMbrs", { timeout: 60000 });
+      cy.url().should("include", "home");
+      cy.contains("Welcome").should("contain.text", "LDAP");
     });
 
-    it('successfully loads', function() {
-        cy.get('@usernameInput').should('be.visible');
+    it("rejects invalid username", function () {
+      cy.get("@usernameInput").type("badUsername");
+      cy.get("@passwordInput").type(validPassword);
+      cy.get("@loginButton").click();
+      cy.get(selectors.errorMessage).should(
+        "contain.text",
+        "Failed to verify e-signature"
+      );
     });
 
-    it('has all three login items visible on the page', function() {
-        cy.get('@usernameInput').should('be.visible')
-        cy.get('@passwordInput').should('be.visible');
-        cy.get('@loginButton').should('be.visible');
+    it("rejects invalid password", function () {
+      cy.get("@usernameInput").type(validUserName);
+      cy.get("@passwordInput").type("badPassword");
+      cy.get("@loginButton").click();
+      cy.get(selectors.errorMessage).should(
+        "contain.text",
+        "Failed to verify e-signature"
+      );
     });
 
-    describe('Login Page _ login', function() {
-        const validUserName = "ldaptest";
-        const validPassword = "drowssap";
-
-        it('can log in to application _ ldaptest', function() {
-            cy.get('@usernameInput').type(validUserName);
-            cy.get('@passwordInput').type(validPassword);
-            cy.get('@loginButton').click();
-            cy.wait('@getAllMbrs', {timeout: 60000});
-            cy.url().should('include', 'home');
-            cy.contains('Welcome').should('contain.text', 'LDAP');
-        });
-
-        it('rejects invalid username', function() {
-            cy.get('@usernameInput').type('badUsername');
-            cy.get('@passwordInput').type(validPassword);
-            cy.get('@loginButton').click();
-            cy.get(selectors.errorMessage).should('contain.text', 'Failed to verify e-signature');
-        });
-
-        it('rejects invalid password', function() {
-            cy.get('@usernameInput').type(validUserName);
-            cy.get('@passwordInput').type('badPassword');
-            cy.get('@loginButton').click();
-            cy.get(selectors.errorMessage).should('contain.text', 'Failed to verify e-signature');
-        });
-
-        it('log in button not enabled until after username and password are entered', function() {
-            cy.get('@loginButton').should('be.disabled');
-            cy.get('@usernameInput').type(validUserName);
-            cy.get('@loginButton').should('be.disabled');
-            cy.get('@passwordInput').type(validPassword);
-            cy.get('@loginButton').should('not.be.disabled');
-            cy.get('@usernameInput').clear();
-            cy.get('@loginButton').should('be.disabled');
-        });
+    it("log in button not enabled until after username and password are entered", function () {
+      cy.get("@loginButton").should("be.disabled");
+      cy.get("@usernameInput").type(validUserName);
+      cy.get("@loginButton").should("be.disabled");
+      cy.get("@passwordInput").type(validPassword);
+      cy.get("@loginButton").should("not.be.disabled");
+      cy.get("@usernameInput").clear();
+      cy.get("@loginButton").should("be.disabled");
     });
+
+    //todo: write tests for logging in as all user types (using the API is adequate to prove access). That way I can bypass the UI in my other tests.
+  });
 });
